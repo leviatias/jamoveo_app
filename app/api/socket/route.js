f364525ad9a0defb566655170624d2222e6504c5
leviatias <shaharleviatias@gmail.com>
2024-09-08 14:45:33 +0300

    Update route.js
    socket

--- app/api/socket/route.js
+++ app/api/socket/route.js
@@ -1,25 +1,24 @@
 import { Server } from 'socket.io';
 
 const SocketHandler = (req, res) => {
-	if (res.socket.server.io) {
-		console.log('Socket is already running');
-	} else {
-		console.log('Socket is initializing');
-		const io = new Server(res.socket.server);
-		res.socket.server.io = io;
-
-		io.on('connection', (socket) => {
-			socket.on('join-session', (sessionId) => {
-				socket.join(sessionId);
-				console.log(`User joined session ${sessionId}`);
-			});
-
-			socket.on('update-current-song', ({ sessionId, songId }) => {
-				io.to(sessionId).emit('song-updated', songId);
-			});
-		});
-	}
-	res.end();
+  if (res.socket.server.io) {
+    console.log('Socket is already running');
+  } else {
+    console.log('Socket is initializing');
+    const io = new Server(res.socket.server);
+    res.socket.server.io = io;
+    io.on('connection', (socket) => {
+      socket.on('join-session', (sessionId) => {
+        socket.join(sessionId);
+        console.log(`User joined session ${sessionId}`);
+      });
+      socket.on('update-current-song', ({ sessionId, songId }) => {
+        io.to(sessionId).emit('song-updated', songId);
+      });
+    });
+  }
+  res.end();
 };
 
-export default SocketHandler;
+export const GET = SocketHandler;
+export const POST = SocketHandler;
